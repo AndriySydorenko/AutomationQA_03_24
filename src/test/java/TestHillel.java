312aeb8030ddcda18cac8886e110fd93e2873387
Andrii Sydorenko <a.sydorenko9595@gmail.com>
2023-05-31 08:53:55 +0300

    homework1 small fix

--- src/test/java/TestHillel.java
+++ src/test/java/TestHillel.java
@@ -6,36 +6,36 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 public class TestHillel extends BaseClass {
-    static String url = "https://ithillel.ua/";
-    static String helpButton = "btn-consultation-hero";
-    static String nameField = "input-name-consultation";
-    static String emailFiield = "input-email-consultation";
-    static String phoneNubler = "input-tel-consultation";
-    static String tgButton = "//button[@data-name='tg']";
-    static String tgField = "//input[@name=\"tg\"]";
-    static String marketingCheckbox = "//form[@id='form-consultation']/descendant::span[@class='checkbox_checkmark']";
-    static String selectCourseBt = "listbox-btn-input-course-consultation";
-    static String allCourses = "//*[@class='listbox_opt-list -scrollbar']/li";
+    static String mainUrl = "https://ithillel.ua/";
+    static By consultButon = By.id("btn-consultation-hero");
+    static By nameField = By.id("input-name-consultation");
+    static By emailFiield = By.id("input-email-consultation");
+    static By phoneNubler = By.id("input-tel-consultation");
+    static By tgButton = By.xpath("//button[@data-name='tg']");
+    static By tgField = By.xpath("//input[@name=\"tg\"]");
+    static By marketingCheckbox = By.xpath("//form[@id='form-consultation']/descendant::span[@class='checkbox_checkmark']");
+    static By selectCourseBt = By.id("listbox-btn-input-course-consultation");
+    static By allCourses = By.xpath("//*[@class='listbox_opt-list -scrollbar']/li");
 
     public static void main(String[] args) throws InterruptedException {
-        driver.get(url);
-        driver.findElement(By.id(helpButton)).click();
-        driver.findElement(By.id(nameField)).sendKeys("Andrii Sydorenko");
-        driver.findElement(By.id(emailFiield)).sendKeys("a.sydorenko9595@gmail.com");
-        driver.findElement(By.id(phoneNubler)).sendKeys("936255436");
-        driver.findElement(By.xpath(tgButton)).click();
-        driver.findElement(By.xpath(tgField)).sendKeys("https://t.me/Andrii_Sy");
+        driver.get(mainUrl);
+        driver.findElement(consultButon).click();
+        driver.findElement(nameField).sendKeys("Andrii Sydorenko");
+        driver.findElement(emailFiield).sendKeys("a.sydorenko9595@gmail.com");
+        driver.findElement(phoneNubler).sendKeys("936255436");
+        driver.findElement(tgButton).click();
+        driver.findElement(tgField).sendKeys("https://t.me/Andrii_Sy");
         Thread.sleep(1000); // додав паузу тому що після натисканання на телеграм батон форма зміює розмір
 
-        driver.findElement(By.id(selectCourseBt)).click();
-        List<WebElement> list = driver.findElements(By.xpath(allCourses));
+        driver.findElement(selectCourseBt).click();
+        List<WebElement> list = driver.findElements(allCourses);
         for (WebElement e : list) {
             if (e.getText().trim().equals("QA Automation — Java")){
                 e.click();
                 break;
             }
         }
-        driver.findElement(By.xpath(marketingCheckbox)).click();
+        driver.findElement(marketingCheckbox).click();
         Thread.sleep(5000);
         driver.quit();
     }
